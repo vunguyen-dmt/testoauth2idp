f1a0e634e8c75cfe85ec9dcd6498e0a0bfffc9a7
Vu Nguyen <vu.nguyen@dmttek.com>
2025-09-15 18:29:07 +0700

    fix bug due to env var load timing.

--- testidp/backends.py
+++ testidp/backends.py
@@ -2,13 +2,18 @@ from django.conf import settings
 from social_core.backends.oauth import BaseOAuth2
 
 class TestIdPOAuth2(BaseOAuth2):
-    name = "testidp"  # appears in URLs: /auth/login/myidp/
-
-    AUTHORIZATION_URL = settings.SOCIAL_AUTH_TESTIDP_AUTHORIZATION_URL
-    ACCESS_TOKEN_URL = settings.SOCIAL_AUTH_TESTIDP_ACCESS_TOKEN_URL
-    USER_DATA_URL = settings.SOCIAL_AUTH_TESTIDP_USER_DATA_URL
+    name = "testidp"  # will show up as /auth/login/testidp/
 
     DEFAULT_SCOPE = ["openid", "email", "profile"]
+
+    def authorization_url(self):
+        return settings.SOCIAL_AUTH_TESTIDP_AUTHORIZATION_URL
+
+    def access_token_url(self):
+        return settings.SOCIAL_AUTH_TESTIDP_ACCESS_TOKEN_URL
+
+    def user_data_url(self):
+        return settings.SOCIAL_AUTH_TESTIDP_USER_DATA_URL
 
     def get_user_details(self, response):
         """Map IdP JSON response to Open edX fields."""
@@ -23,6 +28,6 @@ class TestIdPOAuth2(BaseOAuth2):
     def user_data(self, access_token, *args, **kwargs):
         """Fetch user profile from IdP using access token."""
         return self.get_json(
-            self.USER_DATA_URL,
+            self.user_data_url(),
             headers={"Authorization": f"Bearer {access_token}"}
         )
