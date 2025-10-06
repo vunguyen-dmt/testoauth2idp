a5228c42037963718eaba9133de96b97385bbc87
Vu Nguyen <vu.nguyen@dmttek.com>
2025-10-06 14:26:42 +0700

    t

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="testidp",
+    name="HT",
     version="0.1",
     packages=find_packages(),
     install_requires=["social-auth-core"],
